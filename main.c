597cc231cea5bc3fbc07b7aefc34d4424dbf5291
Amiraliii29 <119691499+Amiraliii29@users.noreply.github.com>
2023-01-23 13:34:24 +0330

    copy,cut and paste added

--- main.c
+++ main.c
@@ -12,6 +12,11 @@ void rek_mkdir(char *path);
 void insert();
 void cat();
 void removestr();
+void copystr();
+void cutstr();
+void pastestr();
+
+char clipboard[1000000];
 
 int main(){
 
@@ -44,6 +49,12 @@ int main(){
         cat();
     else if(!(strcmp(command , "removestr")))
         removestr();
+    else if(!(strcmp(command , "copystr")))
+        copystr();
+    else if(!(strcmp(command , "cutstr")))
+        cutstr();
+    else if(!(strcmp(command , "pastestr")))
+        pastestr();
     }
 
 }
@@ -274,7 +285,10 @@ void cat(){
         else 
             fileName[i] = get;
     }
-    FILE *fp = fopen(fileName , "r");
+    FILE *fp;
+    printf("%s\n" , fileName);
+    if(!(fp = fopen(fileName , "r")))
+        printf("file doesn't exist :|\n");
 
     while ((get = fgetc(fp)) != EOF)
     {
@@ -436,4 +450,389 @@ void removestr(){
 
         fclose(file);
     }
+}
+
+void copystr(){
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    char in;
+    char fileName[100] = {'\0'};
+    in = getchar();
+    if(in == '\"'){
+        getchar();
+        for (int i = 0; ; i++)
+        {
+            in = getchar();
+            if(in == '\"')
+                break;
+            else{
+                fileName[i] = in;
+            }                
+        }
+    }
+    else{
+        in = getchar();
+        for (int i = 0; ; i++)
+        {
+            if(in == '-' || in == ' ')
+                break;
+            else 
+                fileName[i] = in;
+            in = getchar();
+        }
+    }
+
+    while ((in = getchar()) != 's')
+    {
+    }
+    
+    int line , x;
+    scanf(" %d:%d" , &line , &x);
+    while ((in = getchar()) != 'e')
+    {
+    }
+    int size;
+    scanf("%d" , &size);
+    while ((in = getchar()) != '-')
+    {
+    }
+    char direction = getchar();
+    FILE *file;
+    if(!(file = fopen(fileName , "r")))
+        printf("Bro File doesn't Exist :|\n");  
+    file = fopen(fileName , "r");
+    char temp[1000000];
+    int line_counter = 0;
+    char get , lstring[100];
+    int j = 0;
+    if(direction == 'f'){
+        while (line_counter < line - 1)
+        {
+                fscanf(file , "%[^\n]%*c" , lstring);
+                for (int f = 0; f < strlen(lstring); f++)
+                {
+                    temp[j] = lstring[f];
+                    j++;
+                }
+                temp[j] = '\n';
+                j++;
+                line_counter++;
+        }
+        for (int i = 0; i < x; i++)
+        {
+            get = fgetc(file);
+            temp[j] = get;
+            j++;
+        }
+        for (int i = 0; i < 1000000 ; i++)
+        {
+            clipboard[i] = '\0';
+        }
+        for (int i = 0; i < size ; i++)
+        {
+            get = fgetc(file);
+            clipboard[i] = get;
+        }
+        fclose(file);
+        printf("%s\n" , clipboard);
+    }
+    else if(direction == 'b'){
+        char temp1[1000000] , temp2[1000000];
+        int j1 = 0;
+        int line_counter = 0;
+        while (line_counter < line - 1)
+        {
+            char lstring[100] = {'\0'};
+            fscanf(file , "%[^\n]%*c" , lstring);
+            for (int f = 0; f < strlen(lstring); f++)
+            {
+                temp1[j1] = lstring[f];
+                j1++;
+            }
+            temp1[j1] = '\n';
+            j1++;
+            line_counter++;
+        }
+        for (int i = 0; i < x; i++)
+        {
+            get = fgetc(file);
+            temp1[j1] = get;
+            j1++;
+        }
+        int j2 = 0;
+        get = fgetc(file);
+        while (get != EOF)
+        {
+            temp2[j2] = get;
+            j2++;
+            get = fgetc(file);
+        }
+
+        int j3 = 0;
+        for (int i = 0; i < 1000000; i++)
+        {
+            clipboard[i] = '\0';
+        }
+        for (int f1 = strlen(temp1) - size ; f1 < strlen(temp1) ; f1++)
+        {
+            clipboard[j3] = temp1[f1];
+            j3++;
+        }
+        fclose(file);
+    }
+}
+
+void cutstr(){
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    getchar();
+    char in;
+    char fileName[100] = {'\0'};
+    in = getchar();
+    if(in == '\"'){
+        getchar();
+        for (int i = 0; ; i++)
+        {
+            in = getchar();
+            if(in == '\"')
+                break;
+            else{
+                fileName[i] = in;
+            }                
+        }
+    }
+    else{
+        in = getchar();
+        for (int i = 0; ; i++)
+        {
+            if(in == '-' || in == ' ')
+                break;
+            else 
+                fileName[i] = in;
+            in = getchar();
+        }
+    }
+
+    while ((in = getchar()) != 's')
+    {
+    }
+    
+    int line , x;
+    scanf(" %d:%d" , &line , &x);
+    while ((in = getchar()) != 'e')
+    {
+    }
+    int size;
+    scanf("%d" , &size);
+    while ((in = getchar()) != '-')
+    {
+    }
+    char direction = getchar();
+    FILE *file;
+    if(!(file = fopen(fileName , "r")))
+        printf("Bro File doesn't Exist :|\n");  
+    file = fopen(fileName , "r");
+    char temp[1000000];
+    int line_counter = 0;
+    char get , lstring[100];
+    int j = 0;
+    if(direction == 'f'){
+        while (line_counter < line - 1)
+        {
+                fscanf(file , "%[^\n]%*c" , lstring);
+                for (int f = 0; f < strlen(lstring); f++)
+                {
+                    temp[j] = lstring[f];
+                    j++;
+                }
+                temp[j] = '\n';
+                j++;
+                line_counter++;
+        }
+        for (int i = 0; i < x; i++)
+        {
+            get = fgetc(file);
+            temp[j] = get;
+            j++;
+        }
+        for (int i = 0; i < 1000000 ; i++)
+        {
+            clipboard[i] = '\0';
+        }
+        for (int i = 0; i < size ; i++)
+        {
+            get = fgetc(file);
+            clipboard[i] = get;
+        }
+        while ((get = fgetc(file)) != EOF)
+        {
+            temp[j] = get;
+            j++;
+        }
+        fclose(file);
+        file = fopen(fileName , "w");
+        for (int i = 0; i < strlen(temp) ; i++)
+        {
+            fputc(temp[i] , file);
+        }
+        fclose(file);
+    }
+    else if(direction == 'b'){
+        char temp1[1000000] , temp2[1000000];
+        char temp3[2000000] = {'\0'};
+        int j1 = 0;
+        int line_counter = 0;
+        while (line_counter < line - 1)
+        {
+            char lstring[100] = {'\0'};
+            fscanf(file , "%[^\n]%*c" , lstring);
+            for (int f = 0; f < strlen(lstring); f++)
+            {
+                temp1[j1] = lstring[f];
+                j1++;
+            }
+            temp1[j1] = '\n';
+            j1++;
+            line_counter++;
+        }
+        for (int i = 0; i < x; i++)
+        {
+            get = fgetc(file);
+            temp1[j1] = get;
+            j1++;
+        }
+        int j2 = 0;
+        get = fgetc(file);
+        while (get != EOF)
+        {
+            temp2[j2] = get;
+            j2++;
+            get = fgetc(file);
+        }
+
+        int j3 = 0;
+        for (int i = 0; i < strlen(temp1) - size ; i++)
+        {
+            temp3[j3] = temp1[i];
+            j3++;
+        }
+        int j4 = 0;
+        for (int i = 0; i < 1000000 ; i++)
+        {
+            clipboard[i] = '\0';
+        }
+        
+        for (int f1 = strlen(temp1) - size ; f1 < strlen(temp1) ; f1++)
+        {
+            clipboard[j4] = temp1[f1];
+            j4++;
+        }
+        for (int i = 0; i < strlen(temp2); i++)
+        {
+            temp3[j3] = temp2[i];
+            j3++;
+        }
+        fclose(file);
+        file = fopen(fileName , "w");
+        for (int i = 0; i < strlen(temp3); i++)
+        {
+            fputc(temp3[i] , file);
+        }
+        fclose(file);
+    }
+}
+
+void pastestr(){
+    char in;
+    while ((in = getchar()) != 'e')
+    {
+    }
+    char fileName[100] = {'\0'};
+    in = getchar();
+    if(in == '\"'){
+        getchar();
+        for (int i = 0; ; i++)
+        {
+            in = getchar();
+            if(in == '\"')
+                break;
+            else{
+                fileName[i] = in;
+            }                
+        }
+    }
+    else{
+        in = getchar();
+        for (int i = 0; ; i++)
+        {
+            if(in == '-' || in == ' ')
+                break;
+            else 
+                fileName[i] = in;
+            in = getchar();
+        }
+    }
+    printf("---%s---\n" , fileName);
+    do
+    {
+        in = getchar();
+    } while (in != 's');
+    int line , x;
+    scanf(" %d:%d" , &line , &x);
+   printf("---%d : %d---\n" , line , x);
+
+    FILE *file;
+    if(!(file = fopen(fileName , "r")))
+        printf("Bro File doesn't Exist :|\n");
+    char temp[1000000];
+    int line_counter = 0;
+    char get , lstring[100];
+    int j = 0;
+    while (line_counter < line - 1)
+    {
+            //fscanf(file , "%[^\n]" ,lstring);
+            fscanf(file , "%[^\n]%*c" , lstring);
+            for (int f = 0; f < strlen(lstring); f++)
+            {
+                temp[j] = lstring[f];
+                j++;
+            }
+            temp[j] = '\n';
+            j++;
+            //printf("1%s" , lstring);
+            line_counter++;
+    }
+    for (int i = 0; i < x; i++)
+    {
+        get = fgetc(file);
+        //printf("2%c" , get);
+        temp[j] = get;
+        j++;
+    }
+    for (int i = 0; i < strlen(clipboard) ; i++)
+    {
+        temp[j] = clipboard[i];
+        j++;
+    }
+    while (get != EOF)
+    {
+        get = fgetc(file);
+        //printf("4%c" , get);
+        temp[j] = get;
+        j++;
+    }
+    fclose(file);
+    file = fopen(fileName , "w");
+    for (int i = 0; i < strlen(temp); i++)
+    {
+        fputc(temp[i] , file);
+    }
+    fclose(file);
 }
